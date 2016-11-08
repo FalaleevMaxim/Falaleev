87d0612838efeb311fbce5b1e326518ae69239b8
MaximFalaleev <maximfalaleev@mail.ru>
2016-11-08 11:08:50 +0300

    Initial commit

---
+++ src/main/java/ru/test/Controller/MainController.java
@@ -0,0 +1,17 @@
+package ru.test.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/")
+public class MainController {
+    @RequestMapping(value = "a",method = RequestMethod.GET)
+    public ModelAndView main(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("main");
+        return modelAndView;
+    }
+}
